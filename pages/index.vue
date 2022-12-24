060fcf55016dd2fe9be731c4c673fdb2e8fdcb0d
zpzyyzp <zpzyyzp@gmail.com>
2022-12-24 23:32:24 +0900

    shuffle shops

--- pages/index.vue
+++ pages/index.vue
@@ -49,6 +49,7 @@
 
 <script setup>
 import { useDisplay } from 'vuetify'
+import { shuffle } from 'lodash'
 
 useHead({
   title: 'TOP'
@@ -61,12 +62,12 @@ const perPage = 12
 const prefecturesList = ['北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県', '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県']
 
 const { data: shopsAll } = await useFetch('/api/shops')
-
+const shopShuffle = shuffle(shopsAll.value)
 const shopsFiltered = computed(() => {
   if (prefectures.value) {
-    return shopsAll.value.filter(f => f.prefectures === prefectures.value)
+    return shopShuffle.filter(f => f.prefectures === prefectures.value)
   }
-  return shopsAll.value
+  return shopShuffle
 })
 const filteredTotal = computed(() => { return shopsFiltered.value.length })
 const pages = computed(() => { return Math.ceil(filteredTotal.value / perPage) })
