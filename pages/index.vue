ef3ecf794b07cceb186897a0802dde87c174ac91
zpzyyzp <zpzyyzp@gmail.com>
2023-01-20 11:01:11 +0900

    shop with image raise priority

--- pages/index.vue
+++ pages/index.vue
@@ -64,7 +64,10 @@ const perPage = 12
 const prefecturesList = ['北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県', '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県', 'その他']
 
 const { data: shopsAll } = await useFetch('/api/shops')
-const shopShuffle = shuffle(shopsAll.value)
+const shopWithImage = shuffle(shopsAll.value.filter(shop => shop.image === true))
+const shopNoImage = shuffle(shopsAll.value.filter(shop => shop.image !== true))
+const shopShuffle = [].concat(shopWithImage, shopNoImage)
+// const shopShuffle = shuffle(shopsAll.value)
 const shopsFiltered = computed(() => {
   if (prefectures.value) {
     return shopShuffle.filter(f => f.prefectures === prefectures.value)
